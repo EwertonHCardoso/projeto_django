fd37260d68050dbd92dbe641fd435b432a0e85f0
Ewerton Hoffmann Cardoso <ewerton_hoffmann@hotmail.com>
2025-06-26 07:18:45 -0300

    css

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Ewerton Hoffmann Cardoso'})
+    return render(request, 'recipes/pages/home.html', context={'name': 'Recipes'})
 
